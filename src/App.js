328176b44b1779ba672ed4b1defb45a47aab39cd
Vitor Sandes <sandesvitor@gmail.com>
2021-02-01 23:48:39 -0300

    Deploying web site 3

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <div id="content-area">
 
         <div className="app-header">
-          <h1>Galere Seleta</h1>
+          <h1>Galere Seleta!</h1>
         </div>
 
         <div className="app-nav-bar">
